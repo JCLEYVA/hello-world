62c8e5c3d93c99f0018b8b72fffb0a9313fba8cb
JEANETTE LEYVA <jeanettecleyva@outlook.com>
2023-02-23 17:34:50 -0700

    added basic JS

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let time = new Date()
+let currenthour = time.getHours()
+if (currenthour < 12){
+    alert("Good Morning!");
+
+
+} else if (currenthour > 12 && currenthour< 18){
+    alert("Good Afternoon!");
+
+} else (currenthour > 18 && currenthour< 24);
+
+
+let myname = "102 Coffee"; {
+alert("Welcome to" + myname);
+document.write("Welcome to 102 Coffee");
+}
+
+
+let Roastchoice = prompt ("what is your favorite roast? Blonde, Medium, or Dark?");
+console.log(Roastchoice);
+
+document.write("That's our favorite too! Heres our roasts at a glance")
+document.write("Feel free to browse our ethically sourced coffee!")
+
+
+
+
+
+
+
+
+
+
